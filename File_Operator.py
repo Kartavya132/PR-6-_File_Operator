313aad671f7972778fe42ddd5593ae9cb1749d11
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-18 10:30:21 +0530

    Adding menu and improve data fetching

--- File_Operator.py
+++ File_Operator.py
@@ -1,4 +1,5 @@
 import os
+from sys import exit
 from datetime import datetime
 
 ENTRY_DATA = "PR-6-_File_Operator/data.txt"
@@ -15,12 +16,32 @@ def load_data():
     data = []
     temps = []
     if os.path.exists(ENTRY_DATA):
-        with open(ENTRY_DATA, "r") as f:
-            for line_num, line in enumerate(f, start=1):
-                if line_num % 2 == 1:
-                    temps.append(line)
-                else:
-                    temps.append(line)
-                    data.append(temps)
-                    temps = []
-        return
+        try:
+            with open(ENTRY_DATA, "r") as f:
+                for line_num, line in enumerate(f, start=1):
+                    if line_num % 2 == 1:
+                        temps.append(line)
+                    else:
+                        temps.append(line)
+                        data.append(temps)
+                        temps = []
+        except PermissionError or FileNotFoundError:
+            f = open(ENTRY_DATA, "w")
+    else:
+        try:
+            f = open(ENTRY_DATA, "x")
+        except PermissionError or FileExistsError:
+            f = open(ENTRY_DATA, "w")
+
+
+def show():
+    print("-----------------------------------")
+    print("| Welcome to Personal Diary Entry |")
+    print("-----------------------------------\n")
+
+    print("-------Entry Menu-------")
+    print("1. Add the new Entry")
+    print("2. View all Entry")
+    print("3. Search for an Entry")
+    print("4. Delete all the Entries")
+    print("5. Exit")
